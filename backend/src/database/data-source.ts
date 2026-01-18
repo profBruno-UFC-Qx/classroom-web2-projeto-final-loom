75ad07131acbf9ad29729ce403d73c41732b4376
carlosjeferson <Carlosjjs500214@gmail.com>
2026-01-18 19:54:58 -0300

    chore: configura app e conexão com banco de dados

--- backend/src/database/data-source.ts
+++ backend/src/database/data-source.ts
@@ -1,11 +1,11 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "../entities/User";
+import { Artesao } from "../entities/Artesao";
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
   database: "database.sqlite",
+  entities: [User, Artesao],
   synchronize: true,
-  logging: false,
-  entities: [User],
 });
